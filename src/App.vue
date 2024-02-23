1fc3ced1519e5d2b22c413b81b5e1a093c178d9f
VSonb2014781 <sonvancp1@gmail.com>
2024-02-24 01:13:34 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="page">
+    <h1>Hello, Vue.js!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  // Your component options go here
+}
+</script>
+
+<style scoped>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
